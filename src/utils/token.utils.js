89bff9168da3c4e69336b298381be87d2a27c3b5
Mikolaj1921 <przteaml7@gmail.com>
2026-03-31 11:00:36 +0200

    feat: Cookie & Token refactoring, update token.utils

--- src/utils/token.utils.js
+++ src/utils/token.utils.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/index');
 
+// ua: базові налаштування для захищених кук
+const cookieOptions = {
+  httpOnly: true, // захист від XSS - неможливість доступу до куки через JS
+  secure: config.NODE_ENV === 'production', // !тільки HTTPS у продакшені
+  sameSite: 'strict', // захист від CSRF - кросс-сайт запити
+};
+
 // ua: базова функція генерації підписаного токена
 const generateToken = (payload, secret, expiresIn) => {
   return jwt.sign(payload, secret, { expiresIn });
@@ -24,5 +31,31 @@ module.exports = {
     } catch {
       return null;
     }
+  },
+
+  // ua: встановлення токенів у куки браузера
+  sendTokensAsCookies: (res, userId) => {
+    const accessToken = module.exports.generateAccessToken(userId);
+    const refreshToken = module.exports.generateRefreshToken(userId);
+
+    // кука для access токена (15 хв)
+    res.cookie('accessToken', accessToken, {
+      ...cookieOptions,
+      maxAge: 15 * 60 * 1000,
+    });
+
+    // кука для refresh токена (7 днів)
+    res.cookie('refreshToken', refreshToken, {
+      ...cookieOptions,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+
+    return { accessToken, refreshToken };
+  },
+
+  // ua: видалення токенів із кук (для Logout)
+  clearTokensCookies: (res) => {
+    res.clearCookie('accessToken', cookieOptions);
+    res.clearCookie('refreshToken', cookieOptions);
   },
 };
