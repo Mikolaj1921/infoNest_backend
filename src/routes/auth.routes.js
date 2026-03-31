7daf604de95e96894c52e6c178516a490a9f1e5d
Mikolaj1921 <przteaml7@gmail.com>
2026-03-31 12:14:31 +0200

    feat: Update routes auth, add new routes, config protected routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,17 +3,33 @@
 const express = require('express');
 const authController = require('../controllers/auth.controller'); // controller
 const validate = require('../middlewares/validate.middleware'); // middleware для валідації
+const { protect } = require('../middlewares/auth.middleware'); // middleware для захисту роутів
 const {
   registerSchema,
   loginSchema,
-} = require('../validations/auth.validation'); // схеми валідації
+} = require('../validations/auth.validation');
 
 const router = express.Router();
 
 // ua: route реєстрації з zod-валідацією(auth valid)
 router.post('/register', validate(registerSchema), authController.register);
-
 // ua: route логіну з zod-валідацією(auth valid)
 router.post('/login', validate(loginSchema), authController.login);
+// ua: оновлення токенів (без валідації, бо токен у кук - захищено від CSRF)
+router.post('/refresh', authController.refresh);
+
+// Захищені маршрути (for all - accessToken)
+router.use(protect);
+
+// ua: маршрут для виходу з системи
+router.post('/logout', authController.logout);
+
+// ua: маршрут для отримання інформації про поточного користувача
+router.get('/me', (req, res) => {
+  res.status(200).json({
+    success: true,
+    data: { user: req.user },
+  });
+});
 
 module.exports = router;
