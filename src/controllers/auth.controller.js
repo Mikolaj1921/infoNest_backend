89c76a49959ec55fbd18de786189a85c54cce0d4
Mikolaj1921 <przteaml7@gmail.com>
2026-03-31 11:45:17 +0200

    feat: Controller Auth Update, fix: service auth

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,15 +3,21 @@
 // ua: використання сервісу та асинхронного обробника для контролера
 const authService = require('../services/auth.service');
 const asyncHandler = require('../utils/asyncHandler');
+const tokenUtils = require('../utils/token.utils');
 
 // ua: обробка реєстрації - приймає дані, викликає сервіс і повертає результат
 exports.register = asyncHandler(async (req, res) => {
   const result = await authService.register(req.body);
 
+  // ua: встановленя куків в браузер та отримання токенів (якщо треба для логів)
+  tokenUtils.sendTokensAsCookies(res, result.user.id);
+
   res.status(201).json({
     success: true,
     message: 'User registered successfully',
-    data: result,
+    data: {
+      user: result.user,
+    },
   });
 });
 
@@ -20,9 +26,14 @@ exports.login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const result = await authService.login(email, password);
 
+  // ua: куки
+  tokenUtils.sendTokensAsCookies(res, result.user.id);
+
   res.status(200).json({
     success: true,
     message: 'Login successful',
-    data: result,
+    data: {
+      user: result.user,
+    },
   });
 });
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,45 +1,32 @@
-// ua: тут знаходиться логіка авторизації та аутентифікації користувачів.
+const bcrypt = require('bcryptjs');
+const AppError = require('../utils/appError');
+const prisma = require('../config/db');
 
-const bcrypt = require('bcryptjs'); // ua: Бібліотека для хешування паролів
-const AppError = require('../utils/appError'); // ua: Клас для обробки помилок у додатку
-const tokenUtils = require('../utils/token.utils'); // ua: Утиліти для генерації та верифікації токенів
-
-const prisma = require('../config/db'); // ua: імпорт підготовленого клієнта Prisma для роботи з бд
-
+// ua: сервіс для реєстрації та логіну користувачів
 class AuthService {
-  // ua: Логіка реєстрації - user
   async register(userData) {
     const { email, password, name } = userData;
 
-    // перевірка email - наявність юзера з таким email
     const existingUser = await prisma.user.findUnique({ where: { email } });
     if (existingUser) {
       throw new AppError('A user with this email already exists.', 400);
     }
 
-    // bcrypt - хешування пароля (salt 12)
     const hashedPassword = await bcrypt.hash(password, 12);
 
-    // Створення користувача по схемі призми
     const newUser = await prisma.user.create({
       data: {
         email,
         name,
         password: hashedPassword,
       },
-      select: { id: true, email: true, name: true }, // ua: без повтору хешу пароля
+      select: { id: true, email: true, name: true },
     });
 
-    // генерація пари токенів
-    const accessToken = tokenUtils.generateAccessToken(newUser.id);
-    const refreshToken = tokenUtils.generateRefreshToken(newUser.id);
-
-    return { user: newUser, accessToken, refreshToken };
+    return { user: newUser };
   }
 
-  // ua: Логіка входу
   async login(email, password) {
-    // пошук юзера по email
     const user = await prisma.user.findUnique({
       where: { email },
       select: { id: true, email: true, name: true, password: true },
@@ -49,20 +36,14 @@ class AuthService {
       throw new AppError('Incorrect email or password', 401);
     }
 
-    // порівняння пароля з хешем в базі
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
     if (!isPasswordCorrect) {
       throw new AppError('Incorrect email or password', 401);
     }
 
-    // генерація пари токенів
-    const accessToken = tokenUtils.generateAccessToken(user.id);
-    const refreshToken = tokenUtils.generateRefreshToken(user.id);
-
-    // видалення пароля з об'єкта користувача перед поверненням
     delete user.password;
 
-    return { user, accessToken, refreshToken };
+    return { user };
   }
 }
 
