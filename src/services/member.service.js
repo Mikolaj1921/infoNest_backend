2a7c2da50fd03e303e28664a351b264733f65854
Mikolaj1921 <przteaml7@gmail.com>
2026-04-05 22:56:47 +0200

    feat: Membership service config

---
+++ src/services/member.service.js
@@ -0,0 +1,79 @@
+const prisma = require('../config/db');
+const AppError = require('../utils/appError');
+
+class MemberService {
+  // Add user to workspace
+  async addMember(workspaceId, email, role) {
+    // ua: пошук користувача за email
+    const userToAdd = await prisma.user.findUnique({ where: { email } });
+
+    if (!userToAdd) {
+      throw new AppError('User with this email does not exist', 404);
+    }
+
+    // ua: перевірка, чи користувач вже є учасником робочого простору
+
+    const existingMembership = await prisma.workspaceMember.findUnique({
+      where: {
+        workspaceId_userId: {
+          workspaceId,
+          userId: userToAdd.id,
+        },
+      },
+    });
+
+    if (existingMembership) {
+      throw new AppError('User is already a member of this workspace', 400);
+    }
+
+    //  запис у таблиці зв'язку між юзером та робочим простором з ролею
+    return await prisma.userWorkspace.create({
+      data: {
+        userId: userToAdd.id,
+        workspaceId,
+        role,
+      },
+      include: {
+        user: {
+          select: { id: true, name: true, email: true },
+        },
+      },
+    });
+  }
+
+  // update role
+  // ua: Зміна ролі існуючого учасника
+  async updateMemberRole(workspaceId, userId, newRole) {
+    // Не дозволяємо змінювати роль самому собі (щоб Owner не понизив себе випадково)
+    // Цю логіку можна посилити в контролері
+
+    return await prisma.userWorkspace.update({
+      where: {
+        userId_workspaceId: { userId, workspaceId },
+      },
+      data: { role: newRole },
+    });
+  }
+
+  // ua: Видалення учасника з воркспейсу
+  async removeMember(workspaceId, userId) {
+    const membership = await prisma.userWorkspace.findUnique({
+      where: { userId_workspaceId: { userId, workspaceId } },
+    });
+
+    if (!membership) {
+      throw new AppError('Member not found in this workspace', 404);
+    }
+
+    // Заборона видаляти власника (Owner)
+    if (membership.role === 'Owner') {
+      throw new AppError('Cannot remove the Owner of the workspace', 400);
+    }
+
+    return await prisma.userWorkspace.delete({
+      where: { userId_workspaceId: { userId, workspaceId } },
+    });
+  }
+}
+
+module.exports = new MemberService();
