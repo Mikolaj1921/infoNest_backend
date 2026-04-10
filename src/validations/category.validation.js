a2d147557b9640fc0bf7c30528d7c208134d3b42
Mikolaj1921 <przteaml7@gmail.com>
2026-04-10 20:27:08 +0200

    feat: Update category validator, add updateschema

--- src/validations/category.validation.js
+++ src/validations/category.validation.js
@@ -15,4 +15,17 @@ const createCategorySchema = z.object({
   }),
 });
 
-exports = { createCategorySchema };
+const updateCategorySchema = z.object({
+  body: z.object({
+    name: z
+      .string() // ua: перевірка чи рядок
+      .trim() // ua: видаляє пробіли з початку та кінця рядка
+      .min(3, 'Category name must be at least 3 characters long.')
+      .max(30, 'Category name is too long'),
+  }),
+  params: z.object({
+    id: z.string().cuid('Invalid category ID'), // ua: перевірка на валідний id категорії
+  }),
+});
+
+exports = { createCategorySchema, updateCategorySchema };
